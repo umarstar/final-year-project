93986e49042d3eb71fe68c9279257df5d67d76ef
UmarStar <umarstar737@gmail.com>
2024-08-17 16:37:03 +0500

    h

--- src/Component/Form/Form.js
+++ src/Component/Form/Form.js
@@ -1,6 +1,6 @@
 import React, { useState} from 'react';
 import './Form.scss';
-import { Link ,useNavigate} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Form() {
   const [firstname, setfirstname] = useState('');
@@ -8,19 +8,15 @@ function Form() {
   const [email, setemail] = useState('');
   const [phonenumber, setphone] = useState('');
   const [massage, setmassage] = useState('');
-  const [err, seterr] = useState(false);
- const navigate =useNavigate();
+
 //   useEffect(()=>{
 //    senddata()
 // },[])
 
+
 const apiUrl = 'https://backaend-final-year-project.vercel.app';
+
   const senddata= async()=>{
-    console.log(!firstname);
-  if(!firstname || !lastname || !email ||!phonenumber || !massage){
-    seterr(true);
-    return false
-  }
     //  console.log(firstname,lastname,email,phone,massage);
      let result = await fetch(`${apiUrl}/post`,{
       method:"post",
@@ -31,7 +27,6 @@ const apiUrl = 'https://backaend-final-year-project.vercel.app';
      });
      result =await result.json();
      console.log(result);
-     navigate("/");
      localStorage.setItem("user" ,JSON.stringify(result));
   }
 
@@ -50,31 +45,22 @@ const apiUrl = 'https://backaend-final-year-project.vercel.app';
           <div className="input-otr col-lg-6" >
             <label for="fname" className="label-input body-S">First Name</label>
             <input className="input-field"  value={firstname} onChange={(e)=>setfirstname(e.target.value)} type="text" placeholder="First Name" required />
-            {err && !firstname && <span className='invalid-input'>Enter Valid FirstName</span>}
           </div>
           <div className="input-otr col-lg-6" >
             <label for="lname" className="label-input body-S">Last Name</label>
             <input className="input-field"  type="text" value={lastname} onChange={(e)=>setlastname(e.target.value)} placeholder="Last Name" required />
-            {err && !lastname && <span className='invalid-input'>Enter Valid LastName</span>}
-
           </div>
           <div className="input-otr col-lg-6" >
             <label for="email" className="label-input body-S">EMail Address Here</label>
             <input className="input-field"  type="text" placeholder="Your Email Address" value={email} onChange={(e)=>setemail(e.target.value)} required />
-            {err && !email && <span className='invalid-input'>Enter Valid Email</span>}
-
           </div>
           <div className="input-otr col-lg-6">
             <label for="phone" className="label-input body-S">Phone Number</label>
             <input className="input-field"  type="text" placeholder="Your Phone Number" value={phonenumber} onChange={(e)=>setphone(e.target.value)} required />
-            {err && !phonenumber && <span className='invalid-input'>Enter Valid PhoneNumber</span>}
-
           </div>
           <div className="input-otr col-lg-12" >
             <label for="message" className="label-input body-S">Message</label>
             <textarea className="txtarea" placeholder="Message Here"  value={massage} onChange={(e)=>setmassage(e.target.value)}></textarea>
-            {err && !massage && <span className='invalid-input'>Enter Valid Massage</span>}
-
           </div>
           <div className="col-lg-4 mx-auto action-otr" >
             <Link className="btn-1 send-btn primary-btn" onClick={senddata}>submit</Link>
